b0f08c41baabf2abad4c6c1046801ea410d383e6
Anurag chauhan <Anuragchauhan8510@gmail.com>
2025-12-26 23:19:11 +0530

    lottery game

--- react-state/src/App.jsx
+++ react-state/src/App.jsx
@@ -4,7 +4,10 @@ import './App.css'
 import LikeButton from './LikeButton';
 import LudoBoard from './Ludo';
 import TodoList from './todoList';
-
+import Lottery from './Lottery';
+import TicketNum from './TicketNum';
+import Ticket from './Ticket';
+import { sum } from './helper';
 function App() {
   // const [count, setCount] = useState(0)
 
@@ -20,11 +23,28 @@ function App() {
   //      <LudoBoard/>
   //   </>
   // );
-   return (
-     <>
-       <TodoList/>
-     </>
-   );
+  //  return (
+  //    <>
+  //      <TodoList/>
+  //    </>
+  //  );
+  // return (
+  //   <>
+  //     <Lottery/>
+  //   </>
+  // )
+  let winCondition = (ticket) => {
+    // return sum(ticket) === 15;                                win condition for sum of num is 15
+    // return ticket.every((num) => num === ticket[0]);          win condition for all num is same
+    return ticket[0] === 0;                              
+    //         win condition first num is 0
+  }
+
+    return (
+      <>
+        <Lottery n={3} winCondition={winCondition} />
+      </>
+    );
 }
 
 export default App
