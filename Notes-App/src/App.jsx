11303d1ae55fb6f6c4343356b16e35dcd54c2167
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-03-03 15:08:33 +0530

    n

--- Notes-App/src/App.jsx
+++ Notes-App/src/App.jsx
@@ -1,32 +1,45 @@
 import React from 'react'
 
 const App = () => {
+  
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+  }
+  
   return (
-    <div className="h-screen  bg-black text-white">
-      <form className="flex item-start justify-between  p-10 ">
-        <div className="flex w-full item-start gap-4 flex-col">
-          <input
-            type="text"
-            placeholder="Enter Notes Heading"
-            className="px-5 py-2 w-full border-2 rounded "
-          />
-
-          <textarea
-            placeholder="Enter Details"
-            name=""
-            id=""
-            className="px-5 py-2 w-full h-20 border-2 rounded "
-          ></textarea>
-          <button className="bg-white w-full text-black px-5 py-2 rounded ">
-            Add Note
-          </button>
-        </div>
-        <img
-          className="h-52"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5BWj15xplp-o5EEnGyXnIjlQRyKGHR7pDgg&s"
-          alt=""
+    <div className="h-screen lg:flex bg-black text-white">
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+        className="flex gap-4 item-start lg:w-1/2 flex-col p-10 "
+      >
+        <h1 className="text-3xl font-bold">Add Notes</h1>
+        <input
+          type="text"
+          placeholder="Enter Notes Heading"
+          className="px-5 py-2 w-full font-medium border-2 outline-none rounded "
         />
+
+        <textarea
+          placeholder="Enter Details"
+          name=""
+          id=""
+          className="px-5 py-2 w-full h-32 flex items-start font-medium border-2 outline-none rounded flex-row"
+        ></textarea>
+        <button className="bg-white w-full text-black px-5 py-2 font-medium rounded outline-none">
+          Add Note
+        </button>
       </form>
+      <div className="lg:w-1/2 lg:border-l-2 p-10">
+        <h1 className="text-3xl font-bold">Recent Notes</h1>
+        <div className="flex flex-wrap gap-5 mt-5 h-full overflow-auto">
+          <div className="h-52 w-40 rounded-2xl bg-white"></div>
+          <div className="h-52 w-40 rounded-2xl bg-white"></div>
+          <div className="h-52 w-40 rounded-2xl bg-white"></div>
+        </div>
+      </div>
     </div>
   );
 }
