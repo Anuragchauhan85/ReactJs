482110c92333e87465faa0b5f858ddb88d6e0538
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-03-10 23:41:03 +0530

    c

--- Local-Storage/src/App.jsx
+++ Local-Storage/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const App = () => {
+
+  function getdata9() {
+    const response = fetch("https://jsonplaceholder.typicode.com/todos/1");
+    console.log(response);
+  }
+
   return (
-    <div>   </div>
+    <div><button onClick={getdata9}>get data</button>
+    </div>
   )
 }
 
