80ee521951971c4b42123d234e23e6e403a1f8b6
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-03-11 23:46:14 +0530

    a

--- Local-Storage/src/App.jsx
+++ Local-Storage/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import axios from 'axios'
+
 
 const App = () => {
 
-  const getdata =async () => {
-    const response =await fetch("https://jsonplaceholder.typicode.com/todos/1")
-    const data = await response.json()
-    console.log(data);
+  // const getdata =async () => {
+  //   const response =await fetch("https://jsonplaceholder.typicode.com/todos/1")
+  //   const data = await response.json()
+  //   console.log(data);
+  // }
+
+  const getdata = async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos/1",
+    );
+    console.log(response.data);
   }
 
   return (
