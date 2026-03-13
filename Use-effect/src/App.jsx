c03574df87e6bd2de7e081f0c735e7f5413e5c13
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-03-13 13:37:50 +0530

    useEffect

--- Use-effect/src/App.jsx
+++ Use-effect/src/App.jsx
@@ -1,16 +1,43 @@
 import React from 'react'
 import { useEffect } from 'react'
+import {useState} from 'react'
 
 const App = () => {
-  
+
+
+  const [a, setA] = useState(0)
+  const [b, setB] = useState(0);
+
+  function Achanging(){
+    console.log("a is changing")    
+  }
+
+  function Bchanging() {
+    console.log("B is changing");
+  }
+
   useEffect(function () {
+    Achanging()
     console.log('use effect is running...')
-  })
+  },[a])
 
 
   return (
-    <div>App</div>
-  )
+    <div>
+      <h1>A is {a}</h1>
+      <h1>B is {b}</h1>
+      <button
+        onClick={() => {
+          setA(a + 1);
+        }}
+      >a</button>
+      <button
+        onClick={() => {
+          setB(b - 1);
+        }}
+      >b</button>
+    </div>
+  );
 }
 
 export default App
